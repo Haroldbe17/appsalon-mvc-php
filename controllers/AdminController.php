ea91843ebc86001c49010023ae06965c5d8a98de
harold_barrantes <hbarrantese@ucenfotec.ac.cr>
2024-10-26 21:18:40 -0600

    Cambios

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -7,8 +7,6 @@ use MVC\Router;
 
 class AdminController {
     public static function index( Router $router){
-
-
         isAdmin();
 
         $fecha = $_GET['fecha'] ?? date('Y-m-d');
@@ -19,16 +17,14 @@ class AdminController {
             header('Location: /404');
         }
 
-
-
         //Consultar al base de datos
         $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
         $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
         $consulta .= " FROM citas  ";
         $consulta .= " LEFT OUTER JOIN usuarios ";
         $consulta .= " ON citas.usuarioId=usuarios.id  ";
-        $consulta .= " LEFT OUTER JOIN citasServicios ";
-        $consulta .= " ON citasServicios.citaId=citas.id ";
+        $consulta .= " LEFT OUTER JOIN citasservicios ";
+        $consulta .= " ON citasservicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
         $consulta .= " ON servicios.id=citasServicios.servicioId ";
         $consulta .= " WHERE fecha =  '$fecha' ";
